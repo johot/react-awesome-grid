f55c027e45f66e1f5d0df4afdf9b2fdadda38a5c
Johan Otterud <johan.otterud@gmail.com>
2017-08-23 13:35:06 +0200

    First commit!

---
+++ docs/Develop.md
@@ -0,0 +1,4 @@
+# Developing readme `react-awesome-grid`
+
+## Great guide
+<https://medium.com/@BrodaNoel/how-to-create-a-react-component-and-publish-it-in-npm-668ad7d363ce>
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+class Grid extends React.Component {
+  render() {
+    return (
+      <div>Testing testing</div>
+    );
+  }
+}
+export default Grid;
